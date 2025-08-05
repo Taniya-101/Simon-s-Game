b43ee4d372142ef777f8e13946d9b53888a40229
Taniya <taniyaofficial99@gmail.com>
2025-08-05 18:34:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow", "red", "purple", "green"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function() {
+    if(started == false){
+        console.log("Game Started");
+        started = true;
+
+        levelUp();
+    }
+});
+
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 250);  //random btn choose
+}
+
+function userFlash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(function () {
+        btn.classList.remove("userflash");
+    }, 250);  //random btn choose
+}
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    // console.log(randIdx);
+    // console.log(randColor);
+    // console.log(randBtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+
+    gameFlash(randBtn);
+}
+
+function checkAns(idx){
+    // console.log("current level : ", level);
+    // let idx = level - 1;
+    
+    if (userSeq[idx] === gameSeq[idx]) {
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        h2.innerHTML = `Game Over! Your Score was <b>${level}</b> <br> Press any key to start.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function() {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        reset();    
+    }
+}
+
+function btnPress(){
+    // console.log("btn was pressed",this);
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress);
+}
+
+function reset(){
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
+
+
+
+
+
+
+
+// step1: keypress -> game start
+// step2: button flash + level 1
+// game sequence ['yellow','blue','purple' etc]
+// user sequence ['yellow','blue','purple' etc]
+// step3: user press a button -> game checks user press buttons sequence == game flash button sequence. if same then level up and if not same then Game over.
